b61cbacc1aa216e79c90d6594f776525cd9180f0
amf162-ua <amf162@alu.ua.es>
2026-02-19 13:30:00 +0100

    Terminada P03

--- P03-Drivers/drivers/src/test/java/ppss/practica3/CineTest.java
+++ P03-Drivers/drivers/src/test/java/ppss/practica3/CineTest.java
@@ -1,9 +1,16 @@
 package ppss.practica3;
 
+import com.sun.jdi.connect.Connector;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @DisplayName("Tests asociados a la clase Cine")
 class CineTest {
@@ -15,6 +22,7 @@ class CineTest {
     }
 
     @Test
+    @Tag("no_parametrizado")
     public void C1_reservaButacas_should_return_Exception_when_fila_empty_and_want_3(){
         //Arrange
         boolean[] asientos = new boolean[0];
@@ -30,6 +38,7 @@ class CineTest {
     }
 
     @Test
+    @Tag("no_parametrizado")
     void C2_reservaButacas_should_return_false_when_fila_empty_and_want_zero() {
         //Arrange
         boolean[] asientos = new boolean[0];
@@ -44,6 +53,7 @@ class CineTest {
     }
 
     @Test
+    @Tag("no_parametrizado")
     void C3_reservaButacas_should_return_true_when_fila_has_3_seats_free_and_want_2(){
         //Arrange
         boolean[] asientos = {false, false, false, true, true};
@@ -62,6 +72,7 @@ class CineTest {
     }
 
     @Test
+    @Tag("no_parametrizado")
     void C4_reservaButacas_should_return_false_when_no_free_seats_and_want_1(){
         //Arrange
         boolean[] asientos = {true, true, true};
@@ -76,6 +87,66 @@ class CineTest {
         assertAll("Comprobaciones de reserva",
                 () -> assertEquals(esperado, resultado),
                 () -> assertArrayEquals(esperadoArray, asientos)
+        );
+    }
+
+
+    @DisplayName("reservaButacas_")
+    @ParameterizedTest(name = "reservaButacas_[{index}] should be {2} when we want {0} and {3}")
+    @MethodSource("datosC5")
+    @Tag("parametrizado")
+    void C5_reservaButacas(int numButacas, boolean[] fila, boolean esperado, String descripcion){
+        boolean resultado = cine.reservaButacas(fila, numButacas);
+        assertEquals(esperado, resultado, () -> "reservaButacas failed: " + descripcion);
+    }
+
+    private static Stream<Arguments> datosC5() {
+        return Stream.of(
+                Arguments.of(
+                  0,
+                  new boolean[0],
+                  false,
+                  "fila has no seats"
+                ),
+                Arguments.of(
+                      2,
+                      new boolean[]{false, false, false, true, true},
+                      true,
+                      "there ara 2 free seats"
+                ),
+                Arguments.of(
+                        1,
+                        new boolean[]{true, true, true},
+                        false,
+                        "all seats are already reserves"
+                )
+        );
+    }
+
+    @DisplayName("reservaButacas_")
+    @ParameterizedTest(name = "reservaButacas_[{index}] should be {2} when we want {0} and {3}")
+    @MethodSource("datosC6")
+    @Tag("parametrizado")
+    @Tag("tablaB")
+    void C6_reservaButacas(int numButacas, boolean[] fila, boolean esperado, String descripcion){
+        boolean resultado = cine.reservaButacas(fila, numButacas);
+        assertEquals(esperado, resultado, () -> "reservaButacas failed: " + descripcion);
+    }
+
+    private static Stream<Arguments> datosC6() {
+        return Stream.of(
+                Arguments.of(
+                        1,
+                        new boolean[]{false, false, false},
+                        true,
+                        "fila has 3 free seats"
+                ),
+                Arguments.of(
+                        1,
+                        new boolean[]{true},
+                        false,
+                        "there ara no free seats"
+                )
         );
     }
 }
---
+++ P03-Drivers/drivers/src/test/java/ppss/practica3/FicheroTextoTest.java
@@ -0,0 +1,54 @@
+package ppss.practica3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FicheroTextoTest {
+    private FicheroTexto sut;
+    @BeforeEach
+    void setUp() {
+        sut = new FicheroTexto();
+    }
+
+    @Test
+    void C1_contarCaracteres_should_return_Exception_when_file_does_not_exist() {
+        //Arrange
+        String nombre = "ficheroC1.txt";
+        //Act
+        FicheroException exception = assertThrows(
+                FicheroException.class,
+                () -> sut.contarCaracteres(nombre)
+        );
+
+        //Assert
+        assertEquals("ficheroC1.txt (No existe el archivo o el directorio)", exception.getMessage());
+    }
+
+    @Test
+    void C2_contarCaracteres_should_return_4_when_file_has_4_chars() throws FicheroException{
+        //Arrange
+        String nombre = getClass().getClassLoader().getResource("ficheroCorrecto.txt").getPath();
+        int esperado = 4;
+
+        //Act
+        int resultado = sut.contarCaracteres(nombre);
+
+        //Assert
+        assertEquals(esperado, resultado);
+    }
+    @Test
+    @Tag("excluido")
+    void C3_contarCaracteres_should_return_Exception_when_file_cannot_be_read(){
+        Assertions.fail("Test pendiente de implementar");
+    }
+
+    @Test
+    @Tag("excluido")
+    void C4_contarCaracteres_should_return_Exception_when_file_cannot_be_closed(){
+        Assertions.fail("Test pendiente de implementar");
+    }
+}
